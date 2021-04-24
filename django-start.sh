f84f223291f4c71b77338ab1f3dfbf9200a79c85
Mina Demian <minademian9@gmail.com>
2021-04-24 21:27:34 +0200

    Update django-start.sh

--- django-start.sh
+++ django-start.sh
@@ -2,6 +2,5 @@
 
 # Author : Mina D
 django-admin startproject myproject
-cd myproject
-python manage.py startapp myapp
+python myproject/manage.py startapp myapp
 python manage.py migrate #optional for Paiza Cloud IDe
