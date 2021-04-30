97382d727c2dc8f35b676b5ef0dbfd42b1a6e653
Mina D <minademian9@gmail.com>
2021-04-30 14:43:10 +0200

    Added model definition

--- trydjango/employees/models.py
+++ trydjango/employees/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
