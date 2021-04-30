cedb5b14d9d9015ca1dbbad5671dc263abb81faa
Mina D <minademian9@gmail.com>
2021-04-30 14:57:02 +0200

    Updated models

--- trydjango/employees/models.py
+++ trydjango/employees/models.py
@@ -6,3 +6,4 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
+    address = models.CharField(max_length=100, null=True)
