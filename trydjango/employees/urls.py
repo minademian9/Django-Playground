4e5188a0e29d831fe4b0c0b527a14f42d1359682
Mina D <minademian9@gmail.com>
2021-05-03 16:55:48 +0200

    Added template still not working

--- trydjango/employees/urls.py
+++ trydjango/employees/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('result/<int:a_number>', views.results, name='result'),
+    path('temp/', views.temp, name='template'),
+    path('error/', views.error, name='error 404 page'),
 
 ]
--- trydjango/employees/views.py
+++ trydjango/employees/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+
+from django.http import Http404
+
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Employee
 
@@ -12,3 +16,18 @@ def index(request):
 def results(request, a_number):
     response = "You're looking at the results of  %s."
     return HttpResponse(response % a_number)
+
+def temp(request):
+    output = Employee.objects.all()
+    template = loader.get_template('employees/index.html')
+    context = {
+        'my_list': output,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def error(request):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Exception as e:
+        raise Http404(e)
